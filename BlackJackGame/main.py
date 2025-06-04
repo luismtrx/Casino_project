419e287870048582369e17687a8b42f580127141
luismtrx <153965980+luismtrx@users.noreply.github.com>
2025-06-04 15:09:07 -0400

    Add files via upload

---
+++ BlackJackGame/main.py
@@ -0,0 +1,15 @@
+import random
+from cards import cards, shuffle_deck, deal_card
+
+
+userInput = (print("Welcome to blackjack press 1 to start: " ))
+
+while userInput != 0:
+    deal_card() 
+    pirint("Player first hand: ", player_hand)
+    
+    print("Dealer first hand: ", dealer_hand)
+    
+    print ("Player Second hand: ", player_hand)
+    
+    
